a982777115c3c8a8939c2ec2355eed4334148991
Hariom Singh <hariom2083@gmail.com>
2026-03-12 07:05:20 +0530

    let it be

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,41 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { authenticateUser } from './middleware/auth.js';
+
+import chatRoutes from "./routes/chatRoute.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-app.get('/protected-feature', authenticateUser, (req, res) => {
-    // FastAPI usually stores the user identifier in the 'sub' field
-    const userId = req.user.sub; 
-    res.json({ data: `Successfully accessed by user ${userId}` });
+
+
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
+// make our app ready for deployment
 
-
-const PORT = ENV.PORT || 3001;
 
 const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log("Server is connected on PORT: ", PORT);
-        })
-    } catch (error) {
-        console.error("🚒 Error connecting the server", error)
-    }
-}
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
 
 startServer();
